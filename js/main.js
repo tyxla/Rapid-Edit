a3c49f84a23649408c7311df263600a59a60c9e3
Marin Atanasov <tyxla@abv.bg>
2015-03-04 19:40:35 +0200

    enabling/disabling form fields after dropping

--- js/main.js
+++ js/main.js
@@ -4,6 +4,15 @@ jQuery(function($) {
 		helper: 'clone',
 		connectWith: '.field-list',
 		cursor: "move",
+		stop: function(event, ui) {
+			var $item = $(ui.item);
+			var $input = $item.find('.field-trigger');
+			if ($item.parent().hasClass('selected-fields')) {
+				$input.removeAttr('disabled');
+			} else {
+				$input.attr('disabled', 'disabled');
+			}
+		}
 	});
 
 });
